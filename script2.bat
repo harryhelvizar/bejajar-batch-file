7b4a3a4b9c4fb5a99c31114576cbf0722bee9d1b
harryhelvizar <helvizar@gmail.com>
2021-01-31 09:19:09 +0800

    update script2.bat

--- script2.bat
+++ script2.bat
@@ -5,12 +5,26 @@ title variabel
 
 set var1= Hello word
 set var2= My name is Harry
+set var3= Iam Backend Engineer
 
 echo.
-echo.
-echo.
 echo %var1% 
-echo %var2%
+echo %var2% %var3%
+echo.
+
+::variabel type data angka
+set /a a= 5
+set /a b= 5
+set /a hasil= %a% + %b%
+set /a pangkat= %hasil% * 2
+
+echo hasil dari %a% + %b% = %hasil%
+echo hasil pangkat = %pangkat%
+echo.
+
+::variabel input
+set /p input= "Your name is = "
+echo Thanks %input% for stopping by on my blog 
 
 
 pause>nul
